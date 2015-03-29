6737a1104daddea12218e471de201f88f1abd3e1
Eduardo Robles Elvira <edulix@wadobo.com>
2015-03-29 19:45:09 +0200

    adding support for more primitives

---
+++ agora_results/pipes/modifications.py
@@ -0,0 +1,130 @@
+# -*- coding:utf-8 -*-
+
+import re
+import os
+import json
+
+def apply_modifications(data_list, modifications=[], help="this-parameter-is-ignored"):
+    '''
+    Modify questions with different criteria. Example:
+
+    [
+      {
+        "question_index": 0,
+        "action": "withdraw-candidates",
+        "withdrawal_policy": "match-regexp",
+        "regexp": "^(?<!alcaldable:)",
+        "field": "category",
+        "help": "filter all candidates whose category text does not start with 'alcaldable:'"
+      },
+      {
+        "question_index": 0,
+        "action": "modify-tally-type",
+        "set-tally-type": "plurality-at-large"
+      }
+    ]
+    '''
+    def read_config(data):
+        questions_path = os.path.join(data['extract_dir'], "questions_json")
+        with open(questions_path, 'r', encoding="utf-8") as f:
+            return json.loads(f.read())
+
+    def write_config(data, config):
+        questions_path = os.path.join(data['extract_dir'], "questions_json")
+        with open(questions_path, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(config))
+
+    data = data_list[0]
+    qjson = read_config(data)
+
+    for modif in modifications:
+        qindex = modif['question_index']
+        if modif['action'] == "withdraw-candidates":
+            if "withdrawals" not in data:
+                data['withdrawals'] = []
+
+            if modif['policy'] == 'not-match':
+                field = modif['field']
+                for answer in qjson[qindex]['answers']:
+                    if not re.match(modif['regex'], answer[field]):
+                        data['withdrawals'].append(dict(
+                            question_index=qindex,
+                            answer_id=answer['id'],
+                            answer_text=answer['text']))
+
+            elif modif['policy'] == 'match':
+                field = modif['field']
+                for answer in qjson[qindex]['answers']:
+                    if re.match(modif['regex'], answer[field]):
+                        data['withdrawals'].append(dict(
+                            question_index=qindex,
+                            answer_id=answer['id'],
+                            answer_text=answer['text']))
+
+            elif modif['policy'] == 'withdraw-winners-by-name-from-other-question':
+                dest_qindex = modif['dest_question_index']
+                for answer in data['results']['questions'][qindex]['answers']:
+                    if answer["winner_position"] is None:
+                        continue
+
+                    answer_text = answer['text']
+                    for answer2 in qjson[dest_qindex]['answers']:
+                          if answer2["text"] == answer_text:
+                              data['withdrawals'].append(dict(
+                                  question_index=dest_qindex,
+                                  answer_id=answer2['id'],
+                                  answer_text=answer2['text']))
+                              break
+
+        elif modif['action'] == "remove-candidates":
+            if "removed-candidates" not in data:
+                data['removed-candidates'] = []
+
+            if modif['policy'] == 'not-match':
+                field = modif['field']
+                for answer in qjson[qindex]['answers']:
+                    if not re.match(modif['regex'], answer[field]):
+                        data['removed-candidates'].append(dict(
+                            question_index=qindex,
+                            answer_id=answer['id'],
+                            answer_text=answer['text']))
+
+            elif modif['policy'] == 'match':
+                field = modif['field']
+                for answer in qjson[qindex]['answers']:
+                    if re.match(modif['regex'], answer[field]):
+                        data['removed-candidates'].append(dict(
+                            question_index=qindex,
+                            answer_id=answer['id'],
+                            answer_text=answer['text']))
+
+            elif modif['policy'] == 'remove-winners-by-name-from-other-question':
+                dest_qindex = modif['dest_question_index']
+                for answer in data['results']['questions'][qindex]['answers']:
+                    if answer["winner_position"] is None:
+                        continue
+
+                    answer_text = answer['text']
+                    for answer2 in qjson[dest_qindex]['answers']:
+                        if answer2["text"] == answer_text:
+                            data['removed-candidates'].append(dict(
+                                question_index=dest_qindex,
+                                answer_id=answer2['id'],
+                                answer_text=answer2['text']))
+                            break
+
+        elif modif['action'] == "modify-number-of-winners":
+            qjson[qindex]['num_winners'] = modif['num_winners']
+
+        elif modif['action'] == "truncate-votes":
+            if 'truncate-votes' not in data:
+                data['truncate_votes'] = []
+
+            data['truncate_votes'].append(dict(
+                question_index=modif['question_index'],
+                truncate_votes=modif['truncate_votes']
+            ))
+        else:
+            raise Exception("unrecognized-action %s" % modif['action'])
+
+    write_config(data, qjson)
--- agora_results/pipes/results.py
+++ agora_results/pipes/results.py
@@ -16,12 +16,16 @@ def __patcher(tally):
 
     tally.parse_vote = parse_vote_f
 
-def do_tallies(data_list, ignore_invalid_votes=True, print_as_csv=False, question_indexes=None):
+def do_tallies(data_list, ignore_invalid_votes=True, print_as_csv=False,
+               question_indexes=None, reuse_results=False, truncate_votes=None):
     for data in data_list:
       tallies = []
-      questions_path = os.path.join(data['extract_dir'], "questions_json")
-      with open(questions_path, 'r', encoding="utf-8") as f:
-          questions_json = json.loads(f.read())
+      if not reuse_results:
+          questions_path = os.path.join(data['extract_dir'], "questions_json")
+          with open(questions_path, 'r', encoding="utf-8") as f:
+              questions_json = json.loads(f.read())
+      else:
+          questions_json = data['results']['questions']
 
       if 'size_corrections' in data:
           for question in questions_json:
@@ -32,16 +36,28 @@ def do_tallies(data_list, ignore_invalid_votes=True, print_as_csv=False, questio
       if print_as_csv:
           monkey_patcher = __patcher
 
+      if "truncate_votes" in data and truncate_votes is None:
+          truncate_votes = data["truncate_votes"]
       results = agora_tally.tally.do_tally(
           data['extract_dir'],
           questions_json,
           tallies,
           question_indexes=question_indexes,
           ignore_invalid_votes=ignore_invalid_votes,
-          monkey_patcher=monkey_patcher)
+          monkey_patcher=monkey_patcher,
+          truncate_votes=truncate_votes,
+          withdrawals=data.get('withdrawals', []))
+
+      def get_log(tally, index):
+          if question_indexes is not None and index not in question_indexes:
+              if 'log' not in data:
+                  return {}
+              else:
+                  return data['log'][index]
+          return tally.get_log()
 
       data['results'] = results
-      data['log'] = [t.get_log() for t in tallies]
+      data['log'] = [get_log(t, i) for i, t in enumerate(tallies)]
 
 def to_files(data_list, paths):
     i = 0
@@ -53,3 +69,17 @@ def to_files(data_list, paths):
             f.write(json.dumps(data['results'], indent=4, ensure_ascii=False,
                   sort_keys=True, separators=(',', ': ')))
         i += 1
+
+def apply_removals(data_list, help=""):
+    data = data_list[0]
+
+    if "removed-candidates" not in data:
+        return
+
+    removed_list = data["removed-candidates"]
+    for qindex, question in enumerate(data['results']['questions']):
+        q_removed = [
+            removed['answer_id']
+            for removed in removed_list
+            if removed['question_index'] == qindex]
+        question['answers'] = [answer for answer in question['answers'] if answer['id'] not in q_removed]
