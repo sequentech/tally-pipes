2644e2ff6dc193cf2027b078c09e1d303bd0e115
Eduardo Robles Elvira <edulix@agoravoting.com>
2015-07-15 23:59:57 +0200

    fixing the bcnencomu tally method to support cross-question teams

--- agora_results/pipes/bcnencomu.py
+++ agora_results/pipes/bcnencomu.py
@@ -2,15 +2,19 @@
 
 from collections import defaultdict
 
-def team_count_weight_correction(data_list, original_count_weight, team_count_weight, question_index, help=""):
+def team_count_weight_correction(data_list, original_count_weight, team_count_weight, question_indexes, help=""):
     data = data_list[0]
     team_points = defaultdict(int)
+    team_names = defaultdict(list)
 
-    for answer in data['results']['questions'][question_index]['answers']:
-        team_points[answer['category']] += answer['total_count']
+    for question_index in question_indexes:
+        for answer in data['results']['questions'][question_index]['answers']:
+            team_points[answer['category']] += answer['total_count']
+            team_names[answer['category']].append(answer['text'])
 
-    for answer in data['results']['questions'][question_index]['answers']:
-        answer['total_count'] = (
-            answer['total_count']*original_count_weight
-            +
-            team_points[answer['category']]*team_count_weight)
+    for question_index in question_indexes:
+        for answer in data['results']['questions'][question_index]['answers']:
+            answer['total_count'] = (
+                answer['total_count']*original_count_weight
+                +
+                team_points[answer['category']]*team_count_weight)
