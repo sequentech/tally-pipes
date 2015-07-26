6bd92b878bb9066fa27f4424b3654ac4d1ed5847
Eduardo Robles Elvira <edulix@agoravoting.com>
2015-07-26 11:22:47 +0200

    support ephemeral tallies and append_ballots pipe

--- agora_results/pipes/multipart.py
+++ agora_results/pipes/multipart.py
@@ -407,3 +407,72 @@ def multipart_tally_plaintexts_joiner(data_list, mappings, silent=False,
     # tallying, as we have consolidated all the ballots in that one.
     if reverse:
         data_list.reverse()
+
+
+def append_ballots(data_list, ballots, dst_election_id=0,
+    question_num=0, allow_null_votes=False, help=""):
+    '''
+    appends ballots to an election question. This is an example of the votes:
+    [
+      {
+        "Candidate 1": "3",
+        "Candidate 2": "",
+        "Candidate 3": "1",
+        "Candidate 4": "2",
+      }
+    ]
+
+    The previous example adds one vote where the first selected candidate is
+    "Candidate 3", then "Candidate 4", then "Candidate 1".
+    '''
+
+    # preload questions
+    data = data_list[dst_election_id]
+    data['id'] = dst_election_id
+    questions_path = os.path.join(data['extract_dir'], "questions_json")
+    with open(questions_path, 'r', encoding="utf-8") as f:
+        data['questions'] = json.loads(f.read())
+
+    # sanity-check input ballots
+    assert(isinstance(ballots, list))
+
+    answer_text_to_id = dict([(a['text'], a['id']) for a in data['questions'][question_num]['answers']])
+    fill_size = len(str(len(answer_text_to_id) + 2))
+
+    def encode_ballot(ballot, answer_text_to_id, fill_size):
+        # blank vote
+        if len(ballot) == 0:
+            return str(len(answer_text_to_id) + 2).zfill(fill_size)
+
+        inverse_ballot = {str(v): answer_text_to_id[k] for k, v in ballot.items()}
+        return str(int("".join([
+            str(inverse_ballot[str(i)] + 1).zfill(fill_size)
+            for i in range(0,len(inverse_ballot))
+        ])) + 1)
+
+    parsed_ballots = []
+    for ballot in ballots:
+        assert(isinstance(ballot, dict))
+        assert(set(ballot.keys()) == set([a['text'] for a in data['questions'][question_num]['answers']]))
+        for value in ballot.values():
+            assert(isinstance(value, str))
+            # convert to int. all should be int-strings, even null votes, but not empty ones
+            if len(value) > 0:
+                int(value, 10)
+
+        if allow_null_votes:
+            continue
+
+        selection = dict([(k, int(v, 10)-1) for k, v in ballot.items() if len(v) > 0])
+        assert(len(set(selection.values())) == len(selection.values()))
+        # selection should be ints, sequential and starting from 1
+        assert(set(range(0, len(selection))) == set(selection.values()))
+        parsed_ballots.append("\"%s\"\n" % encode_ballot(selection, answer_text_to_id, fill_size))
+
+    dst_plaintexts_path = glob(os.path.join(
+          data_list[dst_election_id]['extract_dir'],
+          "%d*" % question_num, "plaintexts_json"))[0]
+
+    with codecs.open(dst_plaintexts_path, encoding='utf-8', mode='a') as final_plaintexts:
+          for ballot in parsed_ballots:
+              final_plaintexts.write(ballot)
