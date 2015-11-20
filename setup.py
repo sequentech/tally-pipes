389f404111fec0b5d6b87790aac6effcc7e8842d
garridev <manuga93@gmail.com>
2015-11-20 13:49:39 +0100

    Updating setup.py to work properly with pip 7+

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@
 
 from setuptools import setup
 from pip.req import parse_requirements
+from pip.download import PipSession
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt")
+install_reqs = parse_requirements("requirements.txt", session=PipSession())
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
