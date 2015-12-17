f6cbe6d3921bb413611040863cbf29052f35627b
garridev <manuga93@gmail.com>
2015-12-18 00:00:00 +0100

    adding jsonschema dependency

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     install_requires=reqs,
     dependency_links = [
         'git+https://github.com/agoravoting/openstv.git@next#egg=openstv-1.7',
-        'git+https://github.com/agoravoting/agora-tally.git@next#egg=agora-tally-0.0.1'
+        'git+https://github.com/agoravoting/agora-tally.git@next#egg=agora-tally-0.0.1',
+        'git+https://github.com/Julian/jsonschema.git@v2.5.1#egg=jsonschema-2.5.1'
     ]
 )
