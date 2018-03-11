caba975336f9bdd1c75b8708dc01e4a6e4a46b6c
Findeton <felix@nvotes.com>
2018-03-11 09:51:25 +0100

    WIP: add bulk of desborda4 algorithmic code

--- agora_results/pipes/desborda4.py
+++ agora_results/pipes/desborda4.py
@@ -40,8 +40,13 @@ def __get_women_names_from_question(question):
                 break
     return women_names
 
-
-def podemos_desborda3(data_list, women_names=None, question_indexes=None):
+def get_min_num_winners_for_team(category_name, question, desborda_kind):
+    # TODO: IMPLEMENT THIS
+    if 'desborda3' == desborda_kind:
+        return 0
+    return 0
+           
+def podemos_desborda4(data_list, women_names=None, question_indexes=None):
     '''
     Para la votación PODE-17 hay que hacer que en desborda1,2 y 3 se aplique 
     el siguiente algoritmo:
@@ -112,8 +117,70 @@ def podemos_desborda3(data_list, women_names=None, question_indexes=None):
         def get_all_candidates():
             return get_list_by_points(range( num_candidates ))
 
-        def get_min_num_winners_for_team():
+        def get_categories():
+            # total points on the whole round
+            total_points = 0
+            cats = dict()
+            for index, answer in enumerate(question['answers']):
+                cat_name = answer['category']
+                if cat_name not in cats:
+                    cats[cat_name] = dict(
+                        candidates_index = [],  # list of indexes of the candidates from this category
+                        is_minority = False,    # whether the category has less winners than it's mandatory
+                        points_category = 0)
+                cat = cats[cat_name]
+                # add answer index to category
+                cat['candidates_index'].append(index)
+                # add points to category
+                category['points_category'] += answer['total_count']
+                # add total points
+                total_points += answer['total_count']
+             question['totals']['valid_points'] = total_points
+             return cats
+
+        dev get_winners_for_team(category):
+            present_winners = b_unsure[:num_winners]
+            team_winners = [
+                team_member
+                for team_member in category['candidates_index']
+                if team_member in present_winners
+            ]
+            return team_winners
+
+        def insert_min_team_winners(min_num_winners_for_team, category):
+            minority_winners =
+                get_list_by_points(category['candidates_index'])[:min_num_winners_for_team]
+            b_unsure = [j for j in b_unsure if j not in minority_winners]
+            a_sure_winners.extend(minority_winners)
+
+        def move_minority_losers(category):
+            cat_losers = [
+              team_member
+              for team_member in category['candidates_index']
+              if team_member not in a_sure_winners
+            ]
+            b_unsure = [j for j in b_unsure if j not in cat_losers]
+            c_sure_losers.extend(cat_losers)
+
+        def fix_parity():
+            # TODO: IMPLEMENT THIS
+            return True
+
+        def join_lists(a, b, c):
+            l = a.copy()
+            l.extend(b)
+            l.extend(c)
+            return l
+
+        def set_winners_positions(winners):
+            # set the winner_position
+            for aindex, answer in enumerate(question['answers']):
+                if aindex in winners:
+                    answer['winner_position'] = winners.index(aindex)
+                else:
+                    answer['winner_position'] = None
             
+
         num_candidates = len(question['answers'])
         num_winners = question['num_winners']
         a_sure_winners = []
@@ -123,17 +190,18 @@ def podemos_desborda3(data_list, women_names=None, question_indexes=None):
         categories = get_categories()
 
         for category_name, category in categories.items():
-            min_num_winners_for_team = get_min_num_winners_for_team(category_name, category)
-            winners_for_team = get_winners_for_team(category_name, category)
+            min_num_winners_for_team = get_min_num_winners_for_team(category_name, question, 'desborda3')
+            winners_for_team = get_winners_for_team(category)
             if len(winners_for_team) < min_num_winners_for_team:
                 category['is_minority'] = True
             insert_min_team_winners(min_num_winners_for_team, category)
             if category['is_minority']:
                 move_minority_losers(category)
-        fixed = fix_parity(a_sure_winners, b_unsure)
+        fixed = fix_parity()
         if not fixed:
            # mark as not parity-fixed
+           question['parity_fixed'] = False
         full_list = join_lists(a_sure_winners, b_unsure, c_sure_losers)
         final_winners = get_list_by_points(full_list[:num_winners])
         final_losers = get_list_by_points(full_list[: num_candidates - num_winners])
-        mark_winners_and_losers(final_winners, final_losers)
+        set_winners_positions(final_winners)
