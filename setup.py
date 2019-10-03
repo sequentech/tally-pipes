8e827bed0f2bdf227db5ed400b6e78c6ba847616
Eduardo Robles Elvira <edulix@nvotes.com>
2019-10-03 12:43:45 +0200

    allowing pip 9 too

--- setup.py
+++ setup.py
@@ -16,7 +16,11 @@
 # along with agora-results.  If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup
-from pip._internal.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements("requirements.txt")
